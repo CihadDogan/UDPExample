46b8bda5b0d63a12da97aa95795aa0f8900afec5
CihadDogan <cihaddogan1@gmail.com>
2019-09-15 20:13:04 +0300

    Initial commit.

---
+++ UdpTest/Assets/Scripts/UDPReceive.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPReceive : MonoBehaviour
+{
+    // receiving Thread
+    Thread receiveThread;
+
+    // udpclient object
+    UdpClient client;
+
+    UDPSend udpSend;
+
+    // public
+    public string IP = "127.0.0.1";
+    public int port; // define > init
+
+    // start from unity3d
+    public void Start()
+    {
+        udpSend = GetComponent<UDPSend>();
+        init();
+    }
+
+    // init
+    private void init()
+    {
+        // Endpunkt definieren, von dem die Nachrichten gesendet werden.
+        print("UDPSend.init()");
+        print("Sending to 127.0.0.1 : " + port);
+        print("Test-Sending to this Port: nc -u 127.0.0.1  " + port + "");
+
+        client = new UdpClient();
+
+        receiveThread = new Thread(
+            new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+    }
+
+    void OnDisable()
+    {
+        if (receiveThread != null)
+        {
+            receiveThread.Abort();
+        }
+        receiveThread = null;
+
+        client.Close();
+    }
+
+    // receive thread
+    private void ReceiveData()
+    {
+        client = new UdpClient(port);
+        while (true)
+        {
+            try
+            {
+                // Bytes empfangen.
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                byte[] data = client.Receive(ref anyIP);
+
+                // Bytes mit der UTF8-Kodierung in das Textformat kodieren.
+                // string text = Encoding.UTF8.GetString(data);
+
+                Debug.Log("INCOMING DATA : ");
+                for (int i = 0; i < data.Length;i++)
+                    Debug.Log(data[i]);
+
+                int valToSend = ConvertByteArrayToInt(data);
+                udpSend.ValToSend = valToSend;
+            }
+            catch (Exception err)
+            {
+                print(err.ToString());
+            }
+        }
+    }
+
+    int ConvertByteArrayToInt(byte[] array)
+    {
+        int result = 0;
+
+        string inf = "Array : ";
+        for (int i = 0; i < array.Length; i++)
+            inf += array[i].ToString() + ",";
+
+        //Debug.Log(inf);
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            int sp = i * 8;
+            result |= array[i] << sp;
+        }
+
+        double dresult = (double)result / 10000.0f;
+        string sresult = dresult.ToString("0.0000");
+        Debug.Log("<color=red>" + sresult + "</color>");
+
+        return result;
+    }
+}
---
+++ UdpTest/Assets/Scripts/UDPSend.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class UDPSend : MonoBehaviour
+{
+    // prefs
+    public int ValToSend = 1;
+    public string IP;  // define in init
+    public int port;  // define in init
+    public float lastTime;
+
+    // "connection" things
+    IPEndPoint remoteEndPoint;
+    UdpClient client;
+
+    // start from unity3d
+    public void Start()
+    {
+        init();
+    }
+
+    // init
+    public void init()
+    {
+        // Endpunkt definieren, von dem die Nachrichten gesendet werden.
+        print("UDPSend.init()");
+
+        // ----------------------------
+        // Senden
+        // ----------------------------
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), port);
+        client = new UdpClient();
+
+        StartCoroutine(ISend());
+    }
+
+    void OnDisable()
+    {
+        client.Close();
+    }
+
+    IEnumerator ISend()
+    {
+        while (true)
+        {
+            SendMessage(ValToSend);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        yield return null;
+    }
+
+    // sendData
+    private void SendMessage(int val)
+    {
+        try
+        {
+            // Convert string message to byte array.  
+            byte[] serverMessageAsByteArray = BitConverter.GetBytes(val);
+
+            // Den message zum Remote-Client senden.
+            client.Send(serverMessageAsByteArray, serverMessageAsByteArray.Length, remoteEndPoint);
+            //}
+        }
+        catch (Exception err)
+        {
+            print(err.ToString());
+        }
+    }
+}
